9cc5dfd809d894477949efcb1831349718ff2ee7
djjavierc-droid <djjavierc@gmail.com>
2026-06-22 12:43:50 -0600

    fix: metro.config.js

--- artifacts/pdf-tools-mobile/metro.config.js
+++ artifacts/pdf-tools-mobile/metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// Allow HTML files to be bundled as assets (needed for offline PDF.js viewer)
-config.resolver.assetExts.push("html");
-
 module.exports = config;
